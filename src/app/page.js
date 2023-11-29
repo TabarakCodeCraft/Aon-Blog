94832c7391f16ab19eedba2605a3df85824e366f
Tabarak2001 <tabarak.ali1801a@sc.uobaghdad.edu.iq>
2023-11-29 21:01:53 +0300

    task5

--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,57 @@
 "use client"
+
 import { Container } from "./components/container/container";
 import { Header } from "./components/Header/header";
 import styles from './page.module.css';
 import { Card } from "./components/card/card"; 
 import { Footer } from './components/footer/footer'
 import { useEffect, useState } from "react";
-
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Home() {
-
   const [list, setList] = useState([]);
-  const [loding, setLoding] = useState([true]);
+  const [loading, setLoading] = useState(true);
+
   const getBlogs = () => {
-    setLoding(true)
+    setLoading(true);
     fetch("https://api.slingacademy.com/v1/sample-data/blog-posts")
-      .then(res => res.json())
-      .then(data => {
-        setList(data.blogs);
-        setLoding(false)
-      })
+      .then((res) => res.json())
+      .then((data) => {
+       setList((prevList) => [...prevList, ...data.blogs]);
+        setLoading(false);
+      });
   };
 
   useEffect(() => {
-    getBlogs()
-  }, [])
+    getBlogs();
+  }, []);
 
   return (
     <main className={styles.home}>
       <Header />
       <div className={styles.cover}>
-        <div className={styles.overlay}>
-          <Container>
-            <div className={styles.title}><h1>Simple Blog.</h1>
-              <p>A blog created by Aon 2023</p>
-            </div>
-          </Container>
-        </div>
+      <div className={styles.overlay}>
+      <Container>
+      <div className={styles.title}><h1>Simple Blog.</h1>
+      <p>A blog created by Aon 2023</p>
+      </div>
+      </Container>
+      </div>
       </div>
       <Container>
-        {loding &&<div className={styles.loadingContainer}><span class={styles.loader}></span></div>}
-        <div className={styles.grid}>
-          {list.map((el, i) => (
-            <Card key={i} blog={el} />
-          ))}
-        </div>
+        <InfiniteScroll
+          dataLength={list.length}
+          next={getBlogs}
+          hasMore={!loading}
+          loader= {<div className={styles.loadingContainer}>
+            <span className={styles.loader}></span>
+            </div>}>
+          <div className={styles.grid}>
+            {list.map((el, i) => (
+              <Card key={i} blog={el} />
+            ))}
+          </div>
+        </InfiniteScroll>
       </Container>
       <Footer />
     </main>
